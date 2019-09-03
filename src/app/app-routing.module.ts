d48e8de092b6ef5db11bd96d30e6d4346aa5312a
G0-Pr0 <44259552+G0-Pr0@users.noreply.github.com>
2019-09-03 16:06:33 +0200

    login & guard works | working on rest auth featurs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from  './login/login.component';
+import { AppComponent } from './app.component';
+import { AdminGuard } from  './login/login.guard';
+import { HeaderComponent } from './header/header.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { RegisterComponent } from './login/register/register.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './login/verify-email/verify-email.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:  '',
+    component:  AppComponent,
+
+    children: [
+      // [...]
+      
+      {
+        path: 'header',
+        component: HeaderComponent,
+        canActivate: [AdminGuard]
+      },          
+    {
+        path: 'place-order',
+        component: PlaceOrderComponent,
+        canActivate: [AdminGuard]
+    },  
+    {
+        path: 'register',
+        component: RegisterComponent,
+    },
+    {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent,
+    },
+    {
+        path: 'verify-email',
+        component: VerifyEmailComponent,
+    },
+    
+    { 
+        path: 'login', 
+        component: LoginComponent 
+    }
+    ],
+  
+  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { RegisterComponent } from './login/register/register.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './login/verify-email/verify-email.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    PlaceOrderComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'Operarius2'),
+    AngularFirestoreModule, 
+    AngularFireAuthModule, 
     AppRoutingModule
   ],
   providers: [],
