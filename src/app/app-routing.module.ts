848e8b456b0f341160ed0b6ee0456cbdff25330e
toha4991 <37490426+toha4991@users.noreply.github.com>
2019-09-11 20:34:32 +0200

    header updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from  './login/login.component';
-import { AppComponent } from './app.component';
 import { LoginGuard } from  './login/login.guard';
 import { InternalGuard } from  './login/guard/internal.guard';
 import { HeaderComponent } from './header/header.component';
@@ -10,24 +9,28 @@ import { RegisterComponent } from './login/register/register.component';
 import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './login/verify-email/verify-email.component';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 const routes: Routes = [
   {
     path:  '',
     redirectTo: '/login', pathMatch: 'full'
   },
-
-    
-      
       {
         path: 'header',
         component: HeaderComponent,
         canActivate: [LoginGuard]
-      },          
+      },
+      {
+        path: 'navbar',
+        component: NavbarComponent,
+        canActivate: [LoginGuard]
+      },
     {
         path: 'place-order',
         component: PlaceOrderComponent,
         canActivate: [LoginGuard]
-    },  
+    },
     {
         path: 'register',
         component: RegisterComponent,
@@ -43,17 +46,13 @@ const routes: Routes = [
         component: VerifyEmailComponent,
         canActivate: [InternalGuard]
     },
-    
-    { 
-        path: 'login', 
+    {
+        path: 'login',
         component: LoginComponent,
         canActivate: [InternalGuard]
     }
     ,
 ]
-  
-  
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
