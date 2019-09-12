8312f802394e6c0d22c71b7ba4eae2b405ec360e
Bartosz Piekarz <MrLavengar@gmail.com>
2019-09-12 08:35:33 +0200

    create profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ForgotPasswordComponent } from './login/forgot-password/forgot-password
 import { VerifyEmailComponent } from './login/verify-email/verify-email.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     ForgotPasswordComponent,
     VerifyEmailComponent,
     NavbarComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
